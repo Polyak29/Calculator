c74c9a5eb17155da97bb2dc4b13b9ab28359de10
Polyak29 <vladlen4ig_29@mail.ru>
2019-02-13 17:52:57 +0300

    layout 80%

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,64 @@
+'use strict';
 var gulp = require('gulp'),
+    pug = require('gulp-pug'),
     sass = require('gulp-sass'),
-    csso = require('gulp-csso');
+    csso = require('gulp-csso'),
+    autoprefixer = require('gulp-autoprefixer'),
+    sourcemaps = require('gulp-sourcemaps'),
+    notify = require('gulp-notify'),
+    browserSync = require('browser-sync').create();
+
     
+
+gulp.task('serve', function() {
+    browserSync.init({
+        server: {
+            baseDir: "./project"
+        }
+    });
+});
+
+gulp.task('pug', function(){
+    return gulp.src('gulp/pug/*.pug')
+        .pipe(pug({
+            pretty:true
+        }))
+        .pipe(gulp.dest('./project'))
+        .pipe(browserSync.reload({
+            stream:true
+        }))
+});
+ 
+
 gulp.task('sass',(function(){
-    return gulp.src('./**/*/.scss')
-        .pipe(sass())
+    return gulp.src('gulp/sass/*.scss')
+    .pipe(sourcemaps.init())
+        .pipe(sass()
+        .on('error', sass.logError))
+        .pipe(autoprefixer({
+            browsers: ['last 10 versions'],
+            cascade: false
+        }))
+        .on("error", notify.onError({
+            title: "style"
+          }))
         .pipe(csso())
-        .pipe(gulp.dest('./project'));
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest('./project'))
+        .pipe(browserSync.reload({
+            stream:true
+        }))
 
 }));
+
+
+gulp.task('watch', function(){
+    gulp.watch('./**/*/*.pug',gulp.series('pug'))
+    gulp.watch('./**/*/*.scss', gulp.series('sass'))
+});
+
+gulp.task('default', gulp.series(
+    gulp.parallel('pug','sass'),
+    gulp.parallel('watch','serve')
+    ));
     
