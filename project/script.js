515e10164a60987d779026a004cde923c3254161
Polyak29 <vladlen4ig_29@mail.ru>
2019-02-16 18:59:16 +0300

    scripts 30%

--- project/script.js
+++ project/script.js
@@ -1,4 +1,27 @@
-function insert(num){
-   document.getElementsByClassName('display__input--size')[0].value = document.getElementsByClassName('display__input--size')[0].value+num;
     
+function insert(num){
+    var elem = document.getElementsByClassName('display__input--size')[0];
+    var ins = elem.value
+    if(ins==='0'){   
+       ins= num;
+    }else{
+        ins = ins+num 
+    }
+    elem.value=ins;
 }
+function remove(){
+    var elem = document.getElementsByClassName('display__input--size')[0];
+    var value = elem.value;
+    var index = value.length-1;
+    if(index>0){
+        elem.value= value.slice(0,index)
+    }else{
+        elem.value='0';
+    }
+}  
+function clean(){  
+    var elem = document.getElementsByClassName('display__input--size')[0];
+    elem.value='0';
+
+}
+  
