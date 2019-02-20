51cc03fbf6c521f07c0537857835ea368a85db69
Polyak29 <vladlen4ig_29@mail.ru>
2019-02-20 16:53:25 +0300

    scripts 70%

--- gulp/scripts/script.js
+++ gulp/scripts/script.js
@@ -5,9 +5,9 @@ var archive = document.getElementsByClassName('calculator__display-input--shadow
 function insert( btn ) {
     var val = elem.value;
 
-    if (val === '0'){
+    if (val === '0') {
         val = btn;
-    } else {
+    }else {
         val = val + btn; 
     }
 
@@ -43,12 +43,12 @@ function add() {
     if (elem.value.search(/\D/) != '-1') {
         elem.value = elem.value;
         archive.value = archive.value;
-    } else {
-        archive.value = elem.value
+    } 
+    else {
+        archive.value = elem.value;
     }
     operation = elem.value = '+';
 
-    archive.value = archive.value;
                                                                                                                                                                                
 }
 
@@ -63,7 +63,8 @@ function result() {
     }
 
     if (operation === '+') {
-        elem.value = (+firstNumber) + (+secondNumber);
+        sum = (+firstNumber) + (+secondNumber);
+        elem.value = sum;
         archive.value = '';
     } else if (operation === '-') {
         elem.value = (+firstNumber) - (+secondNumber);
@@ -73,6 +74,11 @@ function result() {
         archive.value = '';
     } else if (operation === '÷') {
         elem.value = (+firstNumber) / (+secondNumber);
+        archive.value = '';
+    } else if (operation === 'n') {
+        elem.value = Math.pow(archive.value, elem.value);
+    } else if (operation === '%') {
+        elem.value = eval(+archive.value + z.input + (archive.value * elem.value / 100));
         archive.value = '';
     }
 }
@@ -108,8 +114,39 @@ function toSplit() {
     else {
         archive.value = elem.value;
     }
-
     operation = elem.value = '÷';
-
     archive.value = archive.value;
+}
+
+function exponentiation() {
+    archive.value = elem.value;
+    elem.value = '';
+    operation = elem.value = 'n';
+    elem.value = elem.value.slice(0,-1);
+     
+    
+}
+function change() {
+    if(elem.value === elem.value) {
+        elem.value = -(elem.value);
+    }else {
+        elem. value = elem.value;
+    }
+}
+function fraction() {
+    archive.value = 'NUMBER ' + elem.value 
+    elem.value = 1 / elem.value;
+}
+
+function sqrt(){
+    elem.value = Math.sqrt(elem.value);
+}
+function procent() {
+    z = elem.value.match(/[\+\×\÷\-]/)    
+if( z.input === '+'||'-'||'×'||'÷'){ 
+    elem.value = '';
+
+    }
+    operation = elem.value = '%';
+    elem.value = elem.value.slice(0,-1);
 }
